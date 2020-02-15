12871e6cebc484b2f76b2aa97e80f6027b7b4080
Kelvin Morrison <kelvinninja1@gmail.com>
2020-02-15 05:47:12 +0000

    Update dashboard.php
    support timeline and community area with queries

--- includes/models/dashboard.php
+++ includes/models/dashboard.php
@@ -7,8 +7,14 @@
 // }
 $settlement = DB::fetch("SELECT * FROM `settlement_tb` WHERE `user_id`='$userid'")[0];
 $referals = DB::fetch("SELECT * FROM `referal_tb` WHERE `referee`='$userid' OR `referer`='$userid'");
-
-print_r($referals);
+$dbreferals = DB::query('SELECT referal_tb.referer, referal_tb.referee, referal_tb.status, users_tb.`username` FROM users_tb, referal_tb
+WHERE users_tb.id = referal_tb.referer
+AND referal_tb.referee = :userid
+OR users_tb.id = referal_tb.referee
+AND referal_tb.referer = :userid;', array(':userid'=>$userid));
+// print_r($referals);
+$dbUserActs = DB::query('SELECT id, activity, act_time, status FROM timeline_tb
+WHERE user_id = :userid ORDER BY id DESC;', array(':userid'=>$userid));
 ?>
 
 <?php
