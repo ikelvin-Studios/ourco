4884ae88e04a8315c3a206b503b467e23411e487
Kelvin Morrison <kelvinninja1@gmail.com>
2020-02-28 04:28:59 +0000

    added mustSettle to isolate users who deserve settlement and vice versa

--- includes/models/dashboard.php
+++ includes/models/dashboard.php
@@ -1,11 +1,18 @@
 <?php
+$mustSettle = 'no';
+if($powerstatus['badge'] == '2019') {
+  $mustSettle = (DB::query("SELECT * FROM `settlement_tb` WHERE `user_id`='$userid'")) ? 'yes' : 'no';
+}
 
+if($mustSettle == 'yes') {
+  $settlement = DB::fetch("SELECT * FROM `settlement_tb` WHERE `user_id`='$userid'")[0];
+}
 
 // $requests_no = DB::count("SELECT * FROM `requests_tb` WHERE `from_id` = '$userid' AND `status` = 'issued' OR `to_id` = '$userid' AND `status` = 'confirmed'");
 // if ($requests_no > 0) {
 // $requests_tb = DB::fetch("SELECT * FROM `requests_tb` WHERE `from_id` = '$userid' AND `status` = 'issued' OR `to_id` = '$userid' AND `status` = 'confirmed'");
 // }
-$settlement = DB::fetch("SELECT * FROM `settlement_tb` WHERE `user_id`='$userid'")[0];
+
 $referals = DB::fetch("SELECT * FROM `referal_tb` WHERE `referee`='$userid' OR `referer`='$userid'");
 $dbreferals = DB::query('SELECT referal_tb.referer, referal_tb.referee, referal_tb.status, users_tb.`username` FROM users_tb, referal_tb
 WHERE users_tb.id = referal_tb.referer
