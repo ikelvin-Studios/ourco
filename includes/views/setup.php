5ff481aebb5d2dad32241edc56d1e1e2d0505020
Kelvin Morrison <kelvinninja1@gmail.com>
2020-03-01 08:00:46 +0000

    added dynamic required view for each stage

--- includes/views/setup.php
+++ includes/views/setup.php
@@ -1,5 +1,5 @@
 <?php
-require ('../includes/models/setup.php');
+//require ('../includes/models/setup.php');
 ?>
 
 
@@ -35,81 +35,23 @@ require ('../includes/models/setup.php');
         <div class="p-5">
           <div class="text-center">
             <h1 class="h4 text-gray-900 mb-4">Almost Done, Now Lets Setup Your Acount!</h1>
-            <p class="text-center text-primary">Stage 1/3 Account Setup</p>
+            <p class="text-center text-primary"><?=$setupPageCaption?></p>
             <p class="text-center">Fill all Feild Before clicking SignUp</p>
             <hr class="clean">
           </div>
-          <form class="user" method="post" class="validator-form" action="#create">
-            <div class="form-group row">
-              <div class="col-sm-6 mb-3 mb-sm-0">
-                <input type="text" class="form-control form-control-user" id="exampleFirstName" placeholder="First Name" name="firstNames">
-              </div>
-              <div class="col-sm-6">
-                <input type="text" class="form-control form-control-user" id="exampleLastName" placeholder="Last Name" name="lastNames">
-              </div>
-            </div>
-            <div class="form-group">
-              <input type="email" class="form-control form-control-user" id="exampleInputEmail" placeholder="Email Address" name="email">
-            </div>
-            <div class="form-group">
-              <input type="text" class="form-control form-control-user" id="exampleInputUname" placeholder="Username" name="userNames">
-            </div>
-            <div class="form-group row">
-              <div class="col-sm-6 mb-3 mb-sm-0">
-                <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password" name="password">
-              </div>
-              <div class="col-sm-6">
-                <input type="password" class="form-control form-control-user" id="exampleRepeatPassword" placeholder="Repeat Password" name="password2">
-              </div>
-            </div>
 
-            <div class="form-group row">
-              <div class="col-sm-6 mb-3 mb-sm-0">
-                <input  id="phone" type="tel" class="form-control" name="phone">
-              </div>
-              <div class="col-sm-6">
-                <select class="form-control" id="address-country" name="country"></select>
-              </div>
-              <div class="col-sm-12">
-                <span id="valid-msg" class="hide"></span>
-                <span id="error-msg" class="hide"></span>
-              </div>
-            </div>
-            <div class="form-group">
-              <input type="text" class="form-control form-control-user" id="exampleInputreferer" placeholder="Referer`s ID" name="ref">
-            </div>
-            <div class="form-group">
-              <input type="text" class="form-control form-control-user" id="exampleInputPromo" placeholder="Promo Code" name = "promo">
-            </div>
-            <div class="form-group">
-                                     <label class="cr-styled">
-                                    <input type="checkbox" required>
-
-                                </label>
-                                I have read and I agree with all <a class="" data-toggle="modal" data-target="#PrivillagesModal"> <i class="fa fa-link"></i>
-                                Terms &amp; Conditions</a>
-                                </div>
-
-
-
-
-                            <hr class="dotted">
-
-            <input type="submit" class="btn btn-primary btn-user btn-block" value="Register Account" name="signup"/>
-            <hr>
-            <!-- <a href="index.html" class="btn btn-google btn-user btn-block">
-              <i class="fab fa-google fa-fw"></i> Register with Google
-            </a>
-            <a href="index.html" class="btn btn-facebook btn-user btn-block">
-              <i class="fab fa-facebook-f fa-fw"></i> Register with Facebook
-            </a> -->
-          </form>
+          <?php
+          if (file_exists('../includes/views/'.$setupPage.'.php'))
+            {
+              require_once("../includes/views/$setupPage.php");
+            }
+            ?>
           <hr>
           <!-- <div class="text-center">
             <a class="small" href="recover">Forgot Password?</a>
           </div> -->
           <div class="text-center">
-            <a class="small" href="login">Already have an account? Login!</a>
+            <a class="small" href="#reason">Why is this neccesary?</a>
           </div>
         </div>
       </div>
