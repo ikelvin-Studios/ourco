f66a8c671189365b2e1c61d1f70d601c51c7a64b
Kelvin Morrison <kelvinninja1@gmail.com>
2020-03-03 16:38:07 +0000

    Added labels, gender and dob fields as well as changed name field

--- includes/views/join.php
+++ includes/views/join.php
@@ -39,34 +39,50 @@
             <hr class="clean">
           </div>
           <form class="user" method="post" class="validator-form" action="#create">
+            <div class="form-group">
+              <label class="control-label">Fullname</label>
+                <input type="text" class="form-control form-control-user" id="exampleFirstName" placeholder="Full Name" name="fullNames">
+            </div>
             <div class="form-group row">
-              <div class="col-sm-6 mb-3 mb-sm-0">
-                <input type="text" class="form-control form-control-user" id="exampleFirstName" placeholder="First Name" name="firstNames">
-              </div>
-              <div class="col-sm-6">
-                <input type="text" class="form-control form-control-user" id="exampleLastName" placeholder="Last Name" name="lastNames">
+              <div class="col-sm-6 mt-1">
+                  <label class="control-label">Gender</label>
+                  <select name="gender" class="form-control">
+                    <option value="M">Male</option>
+                    <option value="F">Female</option>
+                    <option value="R">Rather Not Say</option>
+                  </select>
+                </div>
+                <div class="col-sm-6 mt-2">
+                  <label class="control-label">Date Of Birth (DOB)</label>
+                    <input type="date" class="form-control" name="dob" placeholder="Choose Date" value="" required/>
               </div>
             </div>
             <div class="form-group">
+              <label class="control-label">Email</label>
               <input type="email" class="form-control form-control-user" id="exampleInputEmail" placeholder="Email Address" name="email">
             </div>
             <div class="form-group">
+              <label class="control-label">Username/ID</label>
               <input type="text" class="form-control form-control-user" id="exampleInputUname" placeholder="Username" name="userNames">
             </div>
             <div class="form-group row">
               <div class="col-sm-6 mb-3 mb-sm-0">
+                <label class="control-label">Password</label>
                 <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password" name="password">
               </div>
               <div class="col-sm-6">
+                <label class="control-label">Repeat Password</label>
                 <input type="password" class="form-control form-control-user" id="exampleRepeatPassword" placeholder="Repeat Password" name="password2">
               </div>
             </div>
 
             <div class="form-group row">
               <div class="col-sm-6 mb-3 mb-sm-0">
+                <label class="control-label">Phone</label>
                 <input  id="phone" type="tel" class="form-control" name="phone">
               </div>
               <div class="col-sm-6">
+                <label class="control-label">Country</label>
                 <select class="form-control" id="address-country" name="country"></select>
               </div>
               <div class="col-sm-12">
@@ -75,9 +91,11 @@
               </div>
             </div>
             <div class="form-group">
+              <label class="control-label">Who did you hear of us from?</label>
               <input type="text" class="form-control form-control-user" id="exampleInputreferer" placeholder="Referer`s ID" name="ref">
             </div>
             <div class="form-group">
+              <label class="control-label">Have a Promo Code?</label>
               <input type="text" class="form-control form-control-user" id="exampleInputPromo" placeholder="Promo Code" name = "promo">
             </div>
             <div class="form-group">
